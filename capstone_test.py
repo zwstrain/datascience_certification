dbe77839736fcd4afffedf988bb8211ac3d127a9
zwstrain <zwstrain@gmail.com>
2020-09-14 11:03:00 +0200

    Deleting unused files

---
+++ capstone_test.py
@@ -0,0 +1,10 @@
+import sys
+print('Python: {}'.format(sys.version))
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+print("the test was successful")
